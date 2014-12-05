2e62b4d02174c28328dbe80b6d44c7cc7047b564
ZeroHour <wblancha@gmail.com>
2014-12-05 07:34:06 -0500

    dding functionality to remove punctuation

--- html-to-list.py
+++ html-to-list.py
@@ -6,6 +6,6 @@ url = "http://www.oldbaileyonline.org/print.jsp?div=t17800628-33"
 
 response = urllib2.urlopen(url)
 html = response.read()
-text = obo.stripTags(html)
-wordlist = text.split()
+text = obo.stripTags(html).lower()
+wordlist = obo.stripNonAlphaNum(text)
 print wordlist[0:120]
